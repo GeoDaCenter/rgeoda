5cfe86ce60c1e8a474b573fed2b134dbc5d85a6a
Xun Li <xunli@uchicago.edu>
2019-12-04 00:01:28 -0700

    add features for 0.0.4
    pca
    mds (multi dimensional scaling)
    multivariate local geary
    multivariate local join count
    quantile LISA
    NaturalBreaks
    QuantileBreaks
    Hinge15Breaks
    Hinge30Breaks
    PercentileBreaks
    StddevBreaks

---
+++ R/utils.R
@@ -0,0 +1,36 @@
+
+demean <- function(data) {
+  return (gda_demean(data))
+}
+
+standardize_z <- function(data) {
+  return (gda_standardize(data))
+}
+
+standardize_mad <- function(data) {
+  return (gda_standardize_mad(data))
+}
+
+natural_breaks <- function(k, data) {
+  return (gda_naturalbreaks(k, data))
+}
+
+quantile_breaks <- function(k, data) {
+  return (gda_guantilebreaks(k, data))
+}
+
+hinge15_breaks <- function(k, data) {
+  return (gda_hinge15breaks(k, data))
+}
+
+hinge30_breaks <- function(k, data) {
+  return (gda_hinge30breaks(k, data))
+}
+
+percentile_breaks <- function(k, data) {
+  return (gda_percentilebreaks(k, data))
+}
+
+stddev_breaks <- function(k, data) {
+  return (gda_stddevbreaks(k, data))
+}
